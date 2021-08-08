02d51eec3f03ab8e816f5145e722acaf8332fad9
MuhammadHafijur <hafijur.salah@gmail.com>
2021-08-08 08:05:01 +0600

    Getting starte with Swift and this language is beautiful

---
+++ hello.swift
@@ -0,0 +1,16 @@
+print("Hello, world!")
+
+var myVariable = 24
+print("The value is \(myVariable)")
+
+var secondVaribale = 88
+print(secondVaribale);
+
+// this is a comment
+
+/* 
+    this is also a comment,
+    but written over multiple lines
+ */
+
+
