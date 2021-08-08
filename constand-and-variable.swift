97199a2458cc96ebe3be144378b558a417873c8c
MuhammadHafijur <hafijur.salah@gmail.com>
2021-08-08 09:02:08 +0600

    added constant and variable

---
+++ constand-and-variable.swift
@@ -0,0 +1,24 @@
+var a = 32
+print(a)
+
+var b = 40
+b = 88
+// Now "b" has a value of 88
+print(b)
+
+// Multiple variables can be declared on a single line, separated with commas.
+
+
+// Decalaring a constant using the let keyword
+
+let one = 1
+print(one)
+
+/* Declare multiple constants on a single line
+and separate them with commas */
+
+let x = 0.0, y = 0.1, z = 0.4
+print("x is \(x), y is \(y) and z is \(z)")
+
+
+
